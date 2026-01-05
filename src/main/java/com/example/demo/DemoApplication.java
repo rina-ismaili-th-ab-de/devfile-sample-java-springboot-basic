e7963ce6e03a0516d8c077973ff9f793684a353e
rina-ismaili-th-ab-de <115979796+rina-ismaili-th-ab-de@users.noreply.github.com>
2026-01-05 21:14:21 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello OpenShift Version - 2!";
+        throw new RuntimeException("Broken version for rollout test");
     }
 
     public static void main(String[] args) {
