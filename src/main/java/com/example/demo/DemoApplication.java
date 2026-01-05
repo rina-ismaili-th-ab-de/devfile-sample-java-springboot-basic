f9e1cbd8266640fb7c014b7306cf8b92c8e8c8d9
rina-ismaili-th-ab-de <115979796+rina-ismaili-th-ab-de@users.noreply.github.com>
2026-01-05 20:25:36 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello OpenShift from Alma and Rina!";
+        return "Hello OpenShift Version 2!";
     }
 
     public static void main(String[] args) {
